c96775ba0e969d529a3f88feee40c3d90217791d
mch1307 <mch1307@gmail.com>
2017-03-28 23:35:04 +0200

    Update traefik.toml.sh

--- opt/opt/traefik/bin/traefik.toml.sh
+++ opt/opt/traefik/bin/traefik.toml.sh
@@ -20,10 +20,11 @@ TRAEFIK_RANCHER_SECRET_KEY=$(cat $TRAEFIK_RANCHER_SECRET && echo)
 
 TRAEFIK_ENTRYPOINTS_HTTP="\
   [entryPoints.http]
-  address = \":${TRAEFIK_HTTP_PORT}\
-    [entryPoints.http.redirect]\
-    entryPoint = \"https\"\
+  address = \":${TRAEFIK_HTTP_PORT}\"
+    [entryPoints.http.redirect]
+    entryPoint = \"https\"
 "
+
 
 TRAEFIK_ENTRYPOINTS_HTTPS="\
   [entryPoints.https]
